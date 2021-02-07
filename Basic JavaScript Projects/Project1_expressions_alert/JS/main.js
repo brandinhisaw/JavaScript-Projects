daeb672907a867ea7b101d7bad24da063f028881
brandinhisaw <brandin.hisaw@gmail.com>
2021-02-06 19:37:58 -0800

    Add Basic JS Assignment files

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+// variable assignments
+var A = "This is a string"; 
+    B = "This is the "
+    + "\'Right Way\' to put " 
+    + "apostrophes in."; 
+    // an expression concatenating A and B
+    C = A + B;
+
+// change string A's font to blue
+var A = A.fontcolor("blue");
+
+// pop an alert window
+window.alert(B);
+
+// write text to the page
+document.write(A);
+document.write(C);
