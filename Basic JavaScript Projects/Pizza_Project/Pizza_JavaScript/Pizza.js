da92649bba3d299ef9ec36a0c893d658e24f3202
brandinhisaw <brandin.hisaw@gmail.com>
2021-02-16 14:43:05 -0800

    add Pizza.html project
    Pizza menu using HTML, CSS, and JS. Utilizing radio, checkbox, and submit button to take user selections and display back those choices + total cost

---
+++ Basic JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -0,0 +1,67 @@
+// function used to display the selected size and topping choices
+// calculates a cost stored in runningTotal and tracks toppings to display at the end
+function getReceipt() {
+    // initializes the string to be passed from function to function growing into a full receipt
+    var text1 = "<h3>You Ordered:</h3>";
+    var runningTotal = 0; 
+    var sizeTotal = 0;
+    var sizeArray = document.getElementsByClassName("size");
+    // checks for which pizza size radio was selected
+    for (var i = 0; i < sizeArray.length; i++) {
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1 + selectedSize + "<br>";
+        }
+    }
+    // determines cost based on pizza size
+    if (selectedSize === "Personal Pizza") {
+        sizeTotal = 6;
+    } else if (selectedSize === "Small Pizza") {
+        sizeTotal = 8;
+    } else if (selectedSize === "Medium") {
+        sizeTotal = 10;
+    } else if (selectedSize === "Large Pizza") {
+        sizeTotal = 14;
+    } else if (selectedSize === "Extra Large Pizza") {
+        sizeTotal = 16;
+    }
+    // adds the pizza size cost into runningTotal
+    runningTotal = sizeTotal;
+    console.log(selectedSize + " = $" + sizeTotal + ".00");
+    console.log("size text1: " + text1);
+    console.log("subtotal: $" + runningTotal + ".00");
+    // these variables will get passed to each function
+    getTopping(runningTotal, text1);
+};
+
+// function counts the checked toppings to include in totals
+function getTopping(runningTotal, text1) {
+    var toppingTotal = 0;
+    var selectedTopping = [];
+    var toppingArray = document.getElementsByClassName("toppings");
+    // checks each topping and adds the checked ones into the selectedTopping
+    for (var j = 0; j < toppingArray.length; j++) {
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value);
+            console.log("selected topoping item: ("+toppingArray[j].value+")");
+            text1 = text1 + toppingArray[j].value+"<br>";
+        }
+    }
+    // adds chosen toppings to be displaye
+    var toppingCount = selectedTopping.length;
+    if (toppingCount > 1) {
+        toppingTotal = (toppingCount - 1);
+    } else {
+        toppingTotal = 0;
+    }
+    // totals topping cost ($1.00 per - 1 free to start) and adds to runningTotal
+    runningTotal = (runningTotal + toppingTotal);
+    console.log("total selected topping items: " + toppingCount);
+    console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".oo");
+    console.log("topping text1: " + text1);
+    console.log("Purchase Total: " + "$" + runningTotal + ".00");
+    // displays the combined string into showText showing pizza size and selected toppings
+    document.getElementById("showText").innerHTML = text1;
+    // displays the accumulated total cost in totalPrice
+    document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$" + runningTotal + ".00" + "</strong></h3>";
+};
